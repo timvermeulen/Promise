e7ea51afb6d20cfbc5af6123ca269b4ffe829a44
Tim Vermeulen <tvermeulen@me.com>
2018-05-25 18:30:26 +0200

    Use a UUID as Atomic's queue's label

--- Sources/Promise/Atomic.swift
+++ Sources/Promise/Atomic.swift
@@ -4,7 +4,7 @@ final class Atomic<Value> {
     private var _value: Value
     private let queue: DispatchQueue
     
-    init(_ value: Value, queue: DispatchQueue = DispatchQueue(label: "queue")) {
+    init(_ value: Value, queue: DispatchQueue = DispatchQueue(label: UUID().uuidString)) {
         self._value = value
         self.queue = queue
     }
